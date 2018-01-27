1787c5677aa55595f9e8c3a33309c21743d206f6
Francis <frnmesina@gmail.com>
2018-01-27 17:36:18 -0500

    File updates

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,19 @@ gulp.task('default', function() {
 });
 
 gulp.task('html', function() {
-  console.log("Hooray - you created a Gulp task.");
+  console.log("HTML Watch is functioning");
+});
+
+gulp.task('styles', function() {
+  console.log("Styles Watch is working");
 });
 
 gulp.task('watch', function() {
   watch('./app/index.html', function() {
     gulp.start('html');
+  });
 
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('styles');
   });
 });
