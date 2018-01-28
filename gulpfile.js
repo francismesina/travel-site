984ee13113212e3d6c745f3b6dd71a5a00a82efd
Francis <frnmesina@gmail.com>
2018-01-27 21:54:50 -0500

    Section 6, Lec 16 install BrowserSynd

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,33 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
-
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");
 });
 
-gulp.task('html', function() {
-  console.log("HTML Watch is functioning");
-});
-
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
   .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
+
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  })
+
   watch('./app/index.html', function() {
-    gulp.start('html');
+    browserSync.reload();
   });
 
-  watch('./app/assets/styles/**/*.css', function() {
+watch('./app/assets/styles/**/*.css', function() {
     gulp.start('styles');
   });
 });
